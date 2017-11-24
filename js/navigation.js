725ca29e2d6024931500fddca586e4da6ff43437
rhysjparry <rhys.parry@basekit.com>
2017-11-24 11:33:00 +0000

    Updated template for latest dex-21 changes.

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,5 @@
 
 // Custom Toggle
 $(document).on("click", ".navigation-toggle", function() {
-    $( "body, #page-zones__header" ).toggleClass( "navigation-open" );
+    $( "body" ).toggleClass( "navigation-open" );
 });
